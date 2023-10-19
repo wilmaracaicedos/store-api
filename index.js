fdd2eeebda069fa5f3bb869fd94a45cfe12d83f5
Wilmar Caicedo <wilmar.caicedo@farmappweb.com>
2023-10-19 12:59:36 -0500

    agregar caracteristica para consultar datos de usuarios y productos

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const express = require('express');
+const routerApi = require('./routes');
+
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
   res.send('Hola mi server en express');
 });
+
+app.get('/nueva-ruta', (req, res) => {
+  res.send('Hola, soy una nueva ruta');
+});
+
+app.get('/categories/:categoryId/products/:productId', (req, res) => {
+  const { categoryId, productId } = req.params;
+  res.json({
+    categoryId,
+    productId
+  });
+});
+
+routerApi(app);
 
 app.listen(port, () => {
   console.log(`Mi port ${port}`);
