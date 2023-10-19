2ee2bac5b6cbdad777bbc64f005549825a66215c
Wilmar Caicedo <wilmar.caicedo@farmappweb.com>
2023-10-19 17:33:12 -0500

    agregar metodo para crear productos

--- index.js
+++ index.js
@@ -4,20 +4,14 @@ const routerApi = require('./routes');
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Hola mi server en express');
 });
 
 app.get('/nueva-ruta', (req, res) => {
   res.send('Hola, soy una nueva ruta');
-});
-
-app.get('/categories/:categoryId/products/:productId', (req, res) => {
-  const { categoryId, productId } = req.params;
-  res.json({
-    categoryId,
-    productId
-  });
 });
 
 routerApi(app);
