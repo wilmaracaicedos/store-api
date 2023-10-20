43a00ee52e52d0afc2c04318d1803262a75ad090
Wilmar Caicedo <wilmar.caicedo@farmappweb.com>
2023-10-20 14:18:04 -0500

    agregar servicio para la entidad productos

--- routes/products.router.js
+++ routes/products.router.js
@@ -1,19 +1,12 @@
 const express = require('express');
-const { faker } = require('@faker-js/faker');
+
+const ProductsService = require('../services/product.service');
 
 const router = express.Router();
+const service = new ProductsService();
 
-router.get('/', (req, res) => {
-  const products = [];
-  const { size } = req.query;
-  const limit = size || 10;
-  for (let index = 0; index < limit; index++) {
-    products.push({
-      name: faker.commerce.productName(),
-      price: parseInt(faker.commerce.price(), 10),
-      image: faker.image.imageUrl(),
-    });
-  }
+router.get('/', async (req, res) => {
+  const products = await service.find();
   res.json(products);
 });
 
@@ -21,20 +14,43 @@ router.get('/filter', (req, res) => {
   res.send('Yo soy un filter');
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   const { id } = req.params;
-  res.json({
-    id,
-    name: 'Product 1',
-    price: 1000
+  const product = await service.findOne(id);
+  res.status(200).json(product);
+});
+
+router.post('/', async (req, res) => {
+  const body = req.body;
+  const newProduct = await service.create(body);
+  res.status(201).json({
+    message: 'created',
+    data: newProduct,
   });
 });
 
-router.post('/', (req, res) => {
-  const body = req.body;
+router.patch('/:id/', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const body = req.body;
+    const product = await service.update(id, body);
+    res.json({
+      message: 'updated',
+      data: product,
+    });
+  } catch (error) {
+    res.status(404).json({
+      message: error.message,
+    });
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  const { id } = req.params;
+  const rta = await service.delete(id);
   res.json({
-    message: 'created',
-    data: body
+    message: 'deleted',
+    rta,
   });
 });
 
