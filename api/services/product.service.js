01997bc53f108e2580795ca512808d27c7ba981e
Wilmar Caicedo <wilmar.caicedo@farmappweb.com>
2023-10-30 13:59:13 -0500

    agregar caracteristica para filtrar productos

--- api/services/product.service.js
+++ api/services/product.service.js
@@ -2,6 +2,7 @@ const { faker } = require('@faker-js/faker');
 const boom = require('@hapi/boom');
 
 const { models } = require('../libs/sequelize');
+const { Op } = require('sequelize');
 
 class ProductsService {
 
@@ -31,11 +32,25 @@ class ProductsService {
   async find(query) {
     const options = {
       include: ['category'],
+      where : {},
     }
     const { limit, offset } = query;
     if (limit && offset) {
       options.limit = limit;
       options.offset = offset;
+    }
+
+    const { price } = query;
+    if (price) {
+      options.where.price = price;
+    }
+
+    const { price_min, price_max } = query;
+    if (price_min && price_max) {
+      options.where.price = {
+        [Op.gte]: price_min,
+        [Op.lte]: price_max,
+      };
     }
     const products = await models.Product.findAll(options);
     return products;
